2ea42cb45105994bbec977c701f86b32d07dbb39
Chris Barrett <c.barrett@me.com>
2016-08-17 18:58:01 -0700

    added basic math functions to program.cs

---
+++ SimpleCalculator.Tests/ExpressionTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace SimpleCalculator.Tests
+{
+    [TestClass]
+    public class ExpressionTests
+    {
+        [TestMethod]
+        public void ExpressionEnsureICanCreateAnInstance()
+        {
+            Expression expressionCommand = new Expression();
+
+            Assert.IsNotNull(expressionCommand);
+        }
+
+        [TestMethod]
+        public void ExtractingTermsOfExpression()
+        {
+
+            //Arrange
+            Expression term = new Expression();
+
+            //Act
+            int ResultingTerm = term.FirstTerm();
+
+            //Assert
+            Assert.IsTrue(ResultingTerm is ); 
+        }
+    }
+}
--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -10,6 +10,71 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
+
+            var i = 0;
+            string prompt = ">";
+            int counter = i;
+            char[] operatorSymbols = { '+', '-', '/', '*', '%' };
+            bool keepGoing = true;
+
+            while (keepGoing)
+            {
+                Console.Write("[" + counter++ + "]" + prompt + " ");
+
+                string equation = Convert.ToString(Console.ReadLine());
+
+                string[] numbers = equation.Split(operatorSymbols);
+
+
+                List<int> digits = new List<int>();
+                foreach (string n in numbers)
+                {
+                    int digit = Int32.Parse(n);
+                    digits.Add(digit);
+                }
+
+                if (digits.Count != 2)
+                {
+                    Console.WriteLine("YOU GOTTA PUT IN TWO NUMBERS!");
+                }
+
+                if (equation.Contains("+"))
+                {
+                    int answer = digits[0] + digits[1];
+                    Console.WriteLine("   = " + answer);
+                }
+                else if (equation.Contains("-"))
+                {
+                    int answer = digits[0] - digits[1];
+                    Console.WriteLine("   = " + answer);
+                }
+                else if (equation.Contains("/"))
+                {
+                    int answer = digits[0] / digits[1];
+                    Console.WriteLine("   = " + answer);
+                }
+                else if (equation.Contains("*"))
+                {
+                    int answer = digits[0] * digits[1];
+                    Console.WriteLine("   = " + answer);
+                }
+                else
+                {
+                    Console.WriteLine("WHAT AM I SUPPOSED TO DO WITH THAT?!");
+                }
+                
+
+
+
+
+
+
+
+
+            }
+
+
+            Console.ReadKey();
         }
     }
 }
